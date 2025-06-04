cb79e57742b1a1e5bce3c4f6a47a98677ec96849
evander-davis <evanderdavis6@gmail.com>
2025-06-04 13:27:55 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/evander-davis.github.io/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
